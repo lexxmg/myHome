3cd25deee258c5d5dbf3716540d4b0f0bc157620
lexx <lexx.mg@gmail.com>
2020-05-24 15:27:03 +0300

    php полностью работает

--- php/laurent.php
+++ php/laurent.php
@@ -1,4 +1,7 @@
 <?php
+	// состояние JSON http://192.168.0.15/lexx/myHome/php/laurent.php?ip=192.168.0.101
+	// управление http://192.168.0.15/lexx/myHome/php/laurent.php?ip=192.168.0.101&out=4&st=off
+
 	if(count($_GET) == 1) {
 		$res = file_get_contents("http://" . $_GET['ip'] . "/status.xml");
 		$xml = simplexml_load_string($res);
@@ -6,12 +9,25 @@
 	} 
 
 	if(count($_GET) > 1) {
-		echo "больше"
+		$res = file_get_contents("http://" . $_GET['ip'] . "/status.xml");
+		$xml = simplexml_load_string($res);
+		$out = $xml -> out_table0;
+
+		if(!empty($_GET['out']) && !empty($_GET['st'])) {
+			if($_GET['st'] == 'on' && mb_substr($out, $_GET['out'] - 1, 1) == 0) {
+				$resOut = file_get_contents("http://" . $_GET['ip'] . "/server.cgi?data=OUT," . $_GET['out']);
+				echo "$resOut";
+			}
+			
+			if($_GET['st'] == 'off' && mb_substr($out, $_GET['out'] - 1, 1) == 1) {
+				$resOut = file_get_contents("http://" . $_GET['ip'] . "/server.cgi?data=OUT," . $_GET['out']);
+				echo "$resOut";
+			}
+
+			if($_GET['st'] == 'toggle') {
+				$resOut = file_get_contents("http://" . $_GET['ip'] . "/server.cgi?data=OUT," . $_GET['out']);
+				echo "$resOut";
+			}
+		}
 	}
-	//echo $res;
-	//print_r($res);
-	//print_r($xml);
-	//$json = json_encode($xml);
-	
-	//$array = json_decode($json,TRUE);
 ?>
