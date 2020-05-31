92c877fbcdd8583100bf7b2ec7d73b42fbb7a0ae
lexx <lexx.mg@gmail.com>
2020-05-31 17:04:20 +0300

    php read

--- php/set.php
+++ php/set.php
@@ -1,11 +1,13 @@
 <?php
 $arr = array('timer' => $_GET['timer'], 'ip' => $_GET['ip']);
 		echo json_encode($arr);
-		file_put_contents('/lexx/myHome/settings.json', json_encode($arr));
+		file_put_contents('settings.json', json_encode($arr));
 
 //$json = json_decode(file_get_contents('set.json'), true);
 
-echo file_get_contents('/lexx/myHome/settings.json');
-$set = $json['set'];
-$ip = $json['ip'];		
+$j = file_get_contents('log.txt');
+
+echo $j;
+//$set = $json['set'];
+//$ip = $json['ip'];		
 ?>
