5b754fd24970f7db279fd059d0f76e95f06f2670
mac-mini <lexx.mg@gmail.com>
2020-05-31 19:21:31 +0300

    поправил php

--- php/set.php
+++ php/set.php
@@ -1,11 +1,20 @@
 <?php
-$arr = array('timer' => $_GET['timer'], 'ip' => $_GET['ip']);
-		//echo json_encode($arr);
-		file_put_contents('settings.json', json_encode($arr));
+$path = '../settings.json';
+
+if(!isset($_GET)){
+	$arr = array('timer' => $_GET['timer'], 'ip' => $_GET['ip']);
+	file_put_contents($path, json_encode($arr));
+} else {
+	echo file_get_contents($path);
+}
+
+	
+//echo json_encode($arr);
+//file_put_contents('settings.json', json_encode($arr));
 
 //$json = json_decode(file_get_contents('set.json'), true);
 
-echo file_get_contents('settings.json');
+//echo file_get_contents('settings.json');
 //$set = $json['set'];
 //$ip = $json['ip'];		
 ?>
