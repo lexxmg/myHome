7420ef77d7485ee515ffa3b8284fd6d325e25471
mac-book <lexx.mg@gmail.com>
2020-05-31 16:20:56 +0300

    add php and json3

--- php/set.php
+++ php/set.php
@@ -1,11 +1,11 @@
 <?php
 $arr = array('timer' => $_GET['timer'], 'ip' => $_GET['ip']);
 		echo json_encode($arr);
-		file_put_contents('/home/www/lexx/myHome/setting.json', json_encode($arr));
+		file_put_contents('/lexx/myHome/setting.json', json_encode($arr));
 
 //$json = json_decode(file_get_contents('set.json'), true);
 
-echo json_decode(file_get_contents('/home/www/lexx/myHome/setting.json'));
+echo json_decode(file_get_contents('/lexx/myHome/setting.json'));
 $set = $json['set'];
 $ip = $json['ip'];		
 ?>
